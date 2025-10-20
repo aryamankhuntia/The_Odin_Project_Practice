d89461d02ac656302650f15a5cfbc7fac10123de
Aryaman Khuntia <aryaman.khuntia3@gmail.com>
2025-10-21 04:54:05 +0530

    Complete Exercises on JavaScript

--- practice/javascript-exercises/08_removeFromArray/removeFromArray.js
+++ practice/javascript-exercises/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...ele) {
+    const result = [];
+    arr.forEach((item) => 
+    {
+        if(!ele.includes(item)){
+            result.push(item);
+        }
+    });
+    return result;
 };
 
 // Do not edit below this line
--- practice/javascript-exercises/10_leapYears/leapYears.js
+++ practice/javascript-exercises/10_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    const isDivisibleBy4 = year%4 === 0;
+    const isCentury = year%100 === 0;
+    const isDivisibleBy400 = year%400 === 0;
+    if (isDivisibleBy4 && (!isCentury || isDivisibleBy400)){
+        return true;
+    }
+    else{
+        return false;
+    };
 };
 
 // Do not edit below this line
