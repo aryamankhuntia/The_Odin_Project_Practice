7f21daa3f8b9868de302fba25d923f6fe8ea3def
Aryaman Khuntia <aryaman.khuntia3@gmail.com>
2025-10-23 16:25:06 +0530

    Completed some Practice for JS including DOM Manipulation

---
+++ practice/javascript/main.js
@@ -0,0 +1,32 @@
+const container = document.querySelector("#container");
+
+const content = document.createElement("div");
+content.classList.add("content");
+content.textContent = "This is the glorious text-content!";
+
+container.appendChild(content);
+
+const p = document.createElement("p");
+p.textContent = "Hey I'm red!";
+p.style.color = "red";
+container.appendChild(p);
+
+const h3 = document.createElement("h3");
+h3.textContent = "I'm a blue h3!";
+h3.style.color = "blue";
+container.appendChild(h3);
+
+const div = document.createElement("div");
+div.style.backgroundColor = "pink";
+div.style.border = "1px solid black";
+
+const h1 = document.createElement("h1");
+h1.textContent = "I'm in a div";
+
+const p2 = document.createElement("p");
+p2.textContent = "Me Too!";
+
+div.appendChild(h1);
+div.appendChild(p2);
+container.appendChild(div);
+
