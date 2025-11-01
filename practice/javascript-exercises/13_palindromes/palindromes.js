8828eed1f755b923066cd650b482ce4a3723cf82
Aryaman Khuntia <aryaman.khuntia3@gmail.com>
2025-11-01 23:23:47 +0530

    Complete Javascript Exercises in practice folder

--- practice/javascript-exercises/13_palindromes/palindromes.js
+++ practice/javascript-exercises/13_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const alphanum = "abcdefghijklmnopqrstuvwxyz0123456789";
+    const cleanedStr = str
+    .toLowerCase()
+    .split('')
+    .filter((char) => alphanum.includes(char))
+    .join('');
+    for(let i=0;i<=(cleanedStr.length/2);i++){
+        if (cleanedStr[i] === cleanedStr[cleanedStr.length-i-1]){
+            continue;
+        }
+        else{
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
--- practice/javascript-exercises/14_fibonacci/fibonacci.js
+++ practice/javascript-exercises/14_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(numArg) {
+    let num;
+    if (typeof numArg !== "number"){
+        num = parseInt(numArg);
+    }
+    else{
+        num = numArg;
+    }
+    if (num < 0) return "OOPS";
+    if (num == 0) return 0;
+    let a = 0;
+    let b = 1;
+    for(let i=2;i<=num;i++){
+        let curr = a + b;
+        a = b;
+        b = curr;
+    }
+    return b;
 };
 
 // Do not edit below this line
